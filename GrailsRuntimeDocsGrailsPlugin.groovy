0c169912e263a46eb79b28036f066b31f169146f
srinathanantha <srinath.anantha@gmail.com>
2014-06-10 15:57:44 +0530

    Update plugin description.

--- GrailsRuntimeDocsGrailsPlugin.groovy
+++ GrailsRuntimeDocsGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class GrailsRuntimeDocsGrailsPlugin {
     // the plugin version
     def version = "1.0.0"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.x > *"
+    def grailsVersion = "2.0.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
@@ -18,15 +18,14 @@ class GrailsRuntimeDocsGrailsPlugin {
     def authorEmail = "srinath.anantha@gmail.com"
     def title = "Grails Runtime Docs"
     def description = '''\\
-Generates Java and Groovy documentation for a Grails Application from runtime including the dynamically added methods and properties.
-Helps to easily navigate through the Grails artifacts by categorising all the classes into Controllers, Services, Domains, Commands
-and Tag Libraries.
+The Runtime Docs plugin generates both Java and Groovy documentation for a Grails Project. The groovy documentation is generated from run-time thereby facilitating to display even the dynamic methods and properties added to the grails artifacts from various sources. The HTML docs follows Java 7 API documentation template and also adds "Dynamic Method Summary & Dynamic Method Detail" providing the source information of the dynamic methods.
+The plugin also helps to easily navigate through the grails artifacts by further categorizing the classes into Commands, Configs, Controllers, Domains, Services and Tag Libraries.
 '''
     def license = "APACHE"
 
     def issueManagement = [ system: "GIT", url: "https://github.com/srinathanantha/grails-runtime-docs/issues" ]
 
-    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+    def scm = [ url: "https://github.com/srinathanantha/grails-runtime-docs" ]
     
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/grails-runtime-docs"
